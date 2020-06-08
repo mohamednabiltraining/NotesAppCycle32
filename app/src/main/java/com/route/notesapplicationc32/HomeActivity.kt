30f9dc89e7552832045bd131340110204d1293e9
moussa110 <moussam760@gmail.com>
2020-06-08 14:22:58 +0200

    - add recycler view     1-create adapter     2-create note item - list all notes from db in home Activity

--- app/src/main/java/com/route/notesapplicationc32/HomeActivity.kt
+++ app/src/main/java/com/route/notesapplicationc32/HomeActivity.kt
@@ -2,6 +2,10 @@ package com.route.notesapplicationc32
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.route.notesapplicationc32.adapter.NoteAdapter
+import com.route.notesapplicationc32.database.Note
+import com.route.notesapplicationc32.database.NotesDataBase
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -9,15 +13,24 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-/*
-        val db = NotesDataBase.getInstance(applicationContext);
-        val notes = db?.notesDao()?.getAllNotes();
 
-        db?.notesDao()?.insertNote(Note(title="android assignment",desc = "read about singeleton pattern" +
-                "read about migration"))
-        db?.notesDao()?.deleteNoteById(1);
+        val db = NotesDataBase.getInstance(applicationContext)
 
-*/
+//        db?.notesDao()?.insertNote(Note(title="mahmoud moussa",desc="mahmoud moussa",date = "25/10"))
+//        db?.notesDao()?.insertNote(Note(title="android",desc="mahmoud moussa",date = "25/10"))
+//        db?.notesDao()?.insertNote(Note(title="ios",desc="mahmoud moussa",date = "25/10"))
+//        db?.notesDao()?.insertNote(Note(title="full stack",desc="mahmoud moussa",date = "25/10"))
+
+        var note: List<Note>? = db?.notesDao()?.getAllNotes()
+
+        var noteAdapter = NoteAdapter()
+        noteAdapter.changeData(note)
+        home_note_rv.adapter = noteAdapter
+
+
+
+
+
         //Todo: 1- list all notes from db in home Activity
         //Todo :2- create new activity called add note to insert new note to room db
         // Todo: plus points -> swipe to delete note from list
---
+++ app/src/main/java/com/route/notesapplicationc32/adapter/NoteAdapter.kt
@@ -0,0 +1,39 @@
+package com.route.notesapplicationc32.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.route.notesapplicationc32.R
+import com.route.notesapplicationc32.database.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+    private var data: List<Note>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var v = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return data?.size ?: 0
+    }
+
+    fun changeData(item: List<Note>?) {
+        this.data = item
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var model = data?.get(position)
+        holder.title.text = model?.title
+        holder.date.text = model?.date
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView = itemView.findViewById(R.id.item_title_tv)
+        var date: TextView = itemView.findViewById(R.id.item_date_tv)
+    }
+}
